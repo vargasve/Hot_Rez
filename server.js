42a8e28c63b7fabb0fd1a95c978acca2934a5f66
Kevin Pena <kevinpena160@gmail.com>
2018-05-23 14:22:09 -0500

    maybe working

--- server.js
+++ server.js
@@ -6,14 +6,21 @@ var path = require("path");
 // create express server
 var app = express();
 
-//  set port, bitches
+// set port, bitches
 var PORT = process.env.PORT || 3000;
 
 // data parsing 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-var tables = [];
+var tables = [
+  {
+    name: "kevin",
+    number: 4697772814,
+    email: "kevin@gmail.com",
+    id: 34
+  }
+];
 
 app.get("/", function(req, res) {
   res.sendFile(path.join(__dirname, "home.html"));
@@ -31,14 +38,30 @@ app.get("/api/tables", function(req, res) {
   return res.json(tables);
 });
 
+
+
 // create routes 
 // require("./routes/apiRoutes")(app);
 // require("./routes/htmlRoutes")(app);
 
-// I heard you need a listner
+// Create New Characters - takes in JSON input
+app.post("/api/tables", function(req, res) {
+  // req.body hosts is equal to the JSON post sent from the user
+  // This works because of our body-parser middleware
+  var newcharacter = req.body;
 
+  // Using a RegEx Pattern to remove spaces from newCharacter
+  // You can read more about RegEx Patterns later https://www.regexbuddy.com/regex.html
+  newcharacter.name = newcharacter.name.replace(/\s+/g, "").toLowerCase();
 
+  console.log(newcharacter);
 
+  characters.push(newcharacter);
+
+  res.json(newcharacter);
+});
+
+// we need a listner for it to work, duh
 app.listen(PORT, function() {
   console.log("App listening on PORT: " + PORT);
 });
